6cb83ea0434e0c84b9a4992cc3b20a2be3ac9fe5
srinidhigoud <sgm400@nyu.edu>
2018-04-26 00:09:18 -0400

    run

--- Code/runExperiment.s
+++ Code/runExperiment.s
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-
 #SBATCH --verbose
 #SBATCH --job-name=quoraTrainer
 #SBATCH --time=100:00:00
 #SBATCH --nodes=1
-#SBATCH --mem=50GB
+#SBATCH --mem=100GB
 ###SBATCH --partition=gpu
-<<<<<<< HEAD
-#SBATCH --gres=gpu:p1080:1
-#SBATCH --output=out.quoraTrainer.%j
-=======
 #SBATCH --gres=gpu:1
-#SBATCH --output=out.snliTrainer.%j
->>>>>>> 69551a8a869634b5d6e92b66b51d1decaf2613ec
+#SBATCH --output=out.quoraTrainer.%j
+
 
 module load pytorch/python2.7/0.3.0_4
 #module load pytorch/python3.6/0.3.0_4
