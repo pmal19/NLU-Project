9bcffe507aba6d86e862041d34ade7e8b04e371b
PramitMallick <pramitmallick@gmail.com>
2018-04-30 16:11:46 -0400

    fixed inference

--- py_clone/biLSTMs.py
+++ py_clone/biLSTMs.py
@@ -45,8 +45,8 @@ class BiLSTMInference(nn.Module):
         self.batch_size = batch_size
         self.dropout = dropout
         self.embeddings = nn.Embedding(vocab_size, embedding_dim)
-        self.lstmInference = nn.LSTM(input_size=embedding_dim*2, hidden_size=hidden_dim, bidirectional=True)
-        self.hidden2label = nn.Linear(hidden_dim*2, label_size)
+        self.lstmInference = nn.LSTM(input_size=embedding_dim, hidden_size=hidden_dim, bidirectional=True)
+        self.hidden2label = nn.Linear(hidden_dim*4, label_size)
         self.hidden = self.init_hidden()
 
     def init_hidden(self):
@@ -63,9 +63,11 @@ class BiLSTMInference(nn.Module):
         # pdb.set_trace()
         x1 = self.embeddings(sentence1).view(len(sentence1), self.batch_size, -1)
         x2 = self.embeddings(sentence2).view(len(sentence2), self.batch_size, -1)
-        x = torch.cat((x1, x2), 2)
-        lstm_out, self.hidden = self.lstmInference(x, self.hidden)
+        # x = torch.cat((x1, x2), 2)
+        lstm_out1, self.hidden = self.lstmInference(x1, self.hidden)
+        lstm_out2, self.hidden = self.lstmInference(x2, self.hidden)
         # pdb.set_trace()
+        lstm_out = torch.cat((lstm_out1, lstm_out2), 2)
         y = self.hidden2label(lstm_out[-1])
         log_probs = F.log_softmax(y)
         return log_probs
@@ -102,3 +104,35 @@ class BiLSTMDuplicate(nn.Module):
         y = self.hidden2label(lstm_out[-1])
         log_probs = F.log_softmax(y)
         return log_probs
+
+# class BiLSTMCompQuora(nn.Module):
+
+#     def __init__(self, embedding_dim, hidden_dim, vocab_size, label_size, use_gpu, batch_size, dropout=0.5, targetTask="quora", sstPath, enc2Path):
+#         super(BiLSTMDuplicate, self).__init__()
+#         self.hidden_dim = hidden_dim
+#         self.use_gpu = use_gpu
+#         self.batch_size = batch_size
+#         self.dropout = dropout
+#         self.embeddings = nn.Embedding(vocab_size, embedding_dim)
+#         self.lstmDuplicate1 = nn.LSTM(input_size=embedding_dim*2, hidden_size=hidden_dim, bidirectional=True)
+#         self.hidden2label = nn.Linear(hidden_dim*2, label_size)
+#         self.hidden = self.init_hidden()
+
+#     def init_hidden(self):
+#         # first is the hidden h
+#         # second is the cell c
+#         if self.use_gpu:
+#             return (Variable(torch.zeros(2, self.batch_size, self.hidden_dim).cuda()),
+#                     Variable(torch.zeros(2, self.batch_size, self.hidden_dim).cuda()))
+#         else:
+#             return (Variable(torch.zeros(2, self.batch_size, self.hidden_dim)),
+#                     Variable(torch.zeros(2, self.batch_size, self.hidden_dim)))
+
+#     def forward(self, sentence1, sentence2):
+#         x1 = self.embeddings(sentence1).view(len(sentence1), self.batch_size, -1)
+#         x2 = self.embeddings(sentence2).view(len(sentence2), self.batch_size, -1)
+#         x = torch.cat((x1, x2), 2)
+#         lstm_out, self.hidden = self.lstmDuplicate1(x, self.hidden)
+#         y = self.hidden2label(lstm_out[-1])
+#         log_probs = F.log_softmax(y)
+#         return log_probs
