516858df342131a91c80787ea7e47577e098e0f7
PramitMallick <pramitmallick@gmail.com>
2018-05-01 00:24:11 -0400

    sst gpu try

--- py_clone/train_batch_sst.s
+++ py_clone/train_batch_sst.s
@@ -4,7 +4,7 @@
 #SBATCH --job-name=SSTtrain
 #SBATCH --time=100:00:00
 #SBATCH --nodes=1
-#SBATCH --mem=100GB
+#SBATCH --mem=50GB
 ##SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --output=out.SSTtrain.%j
