9c22bfd4b6bc031a85284379c0ab7eefa3ea28b3
srinidhigoud <srinidhi.goud29@gmail.com>
2018-05-02 03:23:13 -0400

    run

--- py_clone/compTrainNLIonQuora.py
+++ py_clone/compTrainNLIonQuora.py
@@ -92,7 +92,7 @@ def train_epoch_progress(model, train_iter, loss_function, optimizer, text_field
     # tot_samples = len(train_iter)*train_iter.batch_size
     # acc = tot_correct/tot_samples
     # pdb.set_trace()
-    return avg_loss, acc
+    return avg_loss, tot_correct
 
 
 def evaluate(model, data, loss_function, name, USE_GPU):
@@ -262,7 +262,7 @@ for epoch in range(EPOCHS):
     tqdm.write('Train: loss %.2f acc %.1f' % (avg_loss, float(acc.data[0])*100./(len(train_iter)*train_iter.batch_size)))
     torch.save(model.state_dict(), out_dir + '/best_model' + '.pth')
     dev_avg_loss, dev_acc = evaluate(model, dev_iter, loss_function, 'Dev', USE_GPU)
-    print('Dev' + ': loss %.2f acc %.1f' % (avg_loss, float(dev_acc.data[0])*100./(len(dev_iter)*dev_iter.batch_size)))    # if dev_acc > best_dev_acc:
+    print('Dev' + ': loss %.2f acc %.1f' % (dev_avg_loss, float(dev_acc.data[0])*100./(len(dev_iter)*dev_iter.batch_size)))    # if dev_acc > best_dev_acc:
     #     if best_dev_acc > 0:
     #         os.system('rm '+ out_dir + '/best_model' + '.pth')
     #     best_dev_acc = dev_acc
