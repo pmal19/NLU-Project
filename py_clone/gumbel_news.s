87b746ba1e7cd2199da995878a1f24553276723b
srinidhigoud <srinidhi.goud29@gmail.com>
2018-05-11 14:56:02 -0400

    all gumbles for all

--- py_clone/gumbel_news.s
+++ py_clone/gumbel_news.s
@@ -8,8 +8,10 @@
 ##SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --output=out.newsGumbel.%j
-source pmm/bin/activate
+# source pmm/bin/activate
+source hmm/bin/activate
 module load pytorch/python2.7/0.3.0_4
 #module load pytorch/python3.6/0.3.0_4
-/home/pm2758/pmm/bin/python ./gumbel_news.py
+# /home/pm2758/pmm/bin/python ./gumbel_news.py
+/home/sgm400/hmm/bin/python ./gumbel_news.py
 
--- py_clone/gumbel_nli.s
+++ py_clone/gumbel_nli.s
@@ -8,7 +8,9 @@
 ##SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --output=out.GumbelNLI.%j
-source activate pmm/bin/activate
+# source activate pmm/bin/activate
+source activate hmm/bin/activate
 module load pytorch/python2.7/0.3.0_4
 #module load pytorch/python3.6/0.3.0_4
-/home/pm2758/pmm/bin/python ./gumbel_nli.py
+# /home/pm2758/pmm/bin/python ./gumbel_nli.py
+/home/sgm400/hmm/bin/python ./gumbel_nli.py
